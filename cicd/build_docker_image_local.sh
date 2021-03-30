3954467583d7f64a99f3db9c6f03f4c387e66bb6
crouchr <richard.crouch100@gmail.com>
2021-03-30 17:45:04 +0100

    Initial commit

--- cicd/build_docker_image_local.sh
+++ cicd/build_docker_image_local.sh
@@ -1,12 +1,10 @@
+# Jenkins runs this to push image to DockerHub
 cd ..
 docker build -t test-service .
 #docker tag test-service registry:5000/test-service:$VERSION
-
-# push to local registry
 #docker push registry:5000/test-service:$VERSION
 
 # push to DockerHub so that it can be pulled into AWS ECS
-
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker tag test-service crouchr/test-service:$VERSION
 docker push crouchr/test-service:$VERSION
