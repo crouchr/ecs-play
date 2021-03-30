26ddbea1043d231e4f10dad2387abd440d4da921
crouchr <richard.crouch100@gmail.com>
2021-03-30 17:20:56 +0100

    Initial commit

--- cicd/build_docker_image.sh
+++ cicd/build_docker_image.sh
@@ -1,5 +1,4 @@
-cd cicd
 docker build -t test-service .
-docker tag test-service registry:5000/test-service:latest
-docker push registry:5000/test-service:latest
-docker rmi test-service:latest
+docker tag test-service registry:5000/test-service:$VERSION
+docker push registry:5000/test-service:$VERSION
+docker rmi test-service
