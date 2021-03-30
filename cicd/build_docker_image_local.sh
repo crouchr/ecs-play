f51ef8b903c68f4ab3a8dfa40782fd007b70ec8e
crouchr <richard.crouch100@gmail.com>
2021-03-30 17:31:25 +0100

    Initial commit

--- cicd/build_docker_image_local.sh
+++ cicd/build_docker_image_local.sh
@@ -1,5 +1,12 @@
 cd ..
 docker build -t test-service .
 docker tag test-service registry:5000/test-service:$VERSION
+
+# push to local registry
 docker push registry:5000/test-service:$VERSION
+
+# push to DockerHub so that it can be pulled into AWS ECS
+docker push test-service:$VERSION
+
+# clean up
 docker rmi test-service
